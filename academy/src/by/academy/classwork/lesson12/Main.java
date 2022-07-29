c9d424c2aa0e8dfc76fd1913ee8868ead9dad11d
Hanna Mikulich <mikulichhanna@gmail.com>
2022-07-29 23:03:51 +0300

    8.Interfaces. Task3.Enum

--- academy/src/by/academy/classwork/lesson12/Main.java
+++ academy/src/by/academy/classwork/lesson12/Main.java
@@ -1,20 +1,31 @@
 package by.academy.classwork.lesson12;
 
 public class Main {
-    public static void main(String[] args){
+	public static void main(String[] args) {
+				
+		//Seasons season = Seasons.valueOf("AUTUMN");
 
-        Seasons[] types = Seasons.values();
-             for (Seasons s: types){
-                System.out.println(s);
-             }
-//      String season = Seasons.SUMMER.name();
-//        if (season)
-//
-//    public void (String season ){
-//        switch (season){
-//            case SPRING: {
-//        }
-//            case SUMMER: {
-//            }
-        }
-    }
+		Seasons favorite = Seasons.valueOf("SUMMER");
+
+		System.out.println("Любимая пора года: " + favorite + ", cредняя температура: " + favorite.temperature);
+		System.out.println(favorite.getTemperature(favorite)); // как вариант
+
+		Seasons[] types = Seasons.values();
+		for (Seasons s : types) {
+			System.out.print(s.name() + ", cредняя температура: " + s.temperature + ". " );
+			s.getDescription();
+			s.findSeason(s);
+		}
+
+	}
+//	3.	а) Создать перечисление, содержащее названия времен года.
+//	б) Создать переменную содержащую ваше любимое время года и распечатать всю информацию о нем. 
+//	в) Создать метод, который принимает на вход переменную созданного вами enum типа.
+//	Если значение равно Лето, выводим на консоль “Я люблю лето” и так далее Используем оператор switch.
+//	г) Перечисление должно содержать переменную, содержащую среднюю температуру в каждом времени года.
+//	д) Добавить конструктор принимающий на вход среднюю температуру.
+//	е) Создать метод getDescription, возвращающий строку “Холодное время года”. 
+//	Переопределить метод getDescription - для константы Лето метод должен возвращать “Теплое время года”.
+//	ж) В цикле распечатать все времена года, среднюю температуру и описание времени года.
+//	
+}
--- academy/src/by/academy/classwork/lesson12/Seasons.java
+++ academy/src/by/academy/classwork/lesson12/Seasons.java
@@ -1,8 +1,48 @@
 package by.academy.classwork.lesson12;
 
 public enum Seasons {
-    WINTER(-5), SPRING(10), SUMMER(20), AUTUMN(5);
+	
+	WINTER(-5),
+	SPRING(+10),
+	SUMMER(+21){
+		@Override
+		void getDescription(){
+			System.out.println("Теплое время года");
+		}
+	}, 
+	
+	AUTUMN(+5);
+	
+	int temperature;
+	
+	Seasons(int temperature) {
+		this.temperature = temperature;
+	}
 
-    Seasons(int temp) {
-    }
-}
+	public int getTemperature(Seasons season) {
+		return temperature;
+	}
+
+	void getDescription(){
+		System.out.println("Холодное время года");
+	}
+
+	public void findSeason(Seasons season) {
+		switch (season) {
+		case SUMMER:
+			System.out.println("I love SUMMER more!");
+			break;
+		case  AUTUMN:
+			System.out.println("I love AUTUMN!");
+			break;
+		case WINTER:
+			System.out.println("I love WINTER!");
+			break;
+		case SPRING:
+			System.out.println("I love SPRING!");
+			break;
+		}
+		
+	}
+	//Seasons.findSummer(season);
+ }
