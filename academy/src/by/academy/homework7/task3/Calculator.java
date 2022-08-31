4e7e47a0d6499eba42c398984f87110336ae1c7a
Hanna Mikulich <mikulichhanna@gmail.com>
2022-08-31 23:15:09 +0300

    task3

---
+++ academy/src/by/academy/homework7/task3/Calculator.java
@@ -0,0 +1,24 @@
+package by.academy.homework7.task3;
+
+public class Calculator {
+	
+	public static int sum( int x,  int y) {
+
+		return x + y;
+	}
+
+	public static double multiply(double x, double y) {
+
+		return x * y;
+	}
+	
+	public static int divide(int x, int y) {
+
+		return x / y;
+	}
+	
+	public static int subtract(int x, int y) {
+
+		return x - y;
+	}
+}
---
+++ academy/src/by/academy/homework7/task3/SuiteTestDemo.java
@@ -0,0 +1,11 @@
+package by.academy.homework7.task3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestDemo.class, Parameterized.class} )  
+public class SuiteTestDemo {
+
+}
