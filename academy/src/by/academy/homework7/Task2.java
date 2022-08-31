9725127cc2a26dc5f1e678e6d1a21addd2655006
Hanna Mikulich <mikulichhanna@gmail.com>
2022-08-31 20:32:05 +0300

    Task2

---
+++ academy/src/by/academy/homework7/Task2.java
@@ -0,0 +1,52 @@
+package by.academy.homework7;
+
+import java.lang.reflect.*;
+import java.util.Arrays;
+
+// Вывести с помощью рефлексии все поля и методы, сначала через 
+// getMethod("name")  getMethods() getField("name") getFields()
+
+public class Task2 {
+	public static void main(String[] args) throws Exception, SecurityException {
+		
+		User user1 = new User();
+		Class<User> userClass = User.class;
+		
+		Method[] methods = userClass.getMethods();
+		System.out.print(" Methods: ");
+		System.out.println(Arrays.toString(methods));
+						
+		Field oneField = userClass.getField("login");
+		System.out.println("oneField: " + oneField);
+		
+		Field[] fields = userClass.getFields();
+		System.out.println(Arrays.toString(fields));
+		
+				
+		Method declaredMethod1 = userClass.getDeclaredMethod("getEmail");
+		System.out.println("declaredMethod1: " + declaredMethod1);
+		
+		Method [] allDecMethods = userClass.getDeclaredMethods();
+		System.out.print(" DeclaredMethods: ");
+		System.out.println(Arrays.toString(allDecMethods));
+		
+		Field decField = userClass.getDeclaredField("email");
+		System.out.println("declared field: " + decField);
+		
+		Field[] declaredFields = userClass.getDeclaredFields();
+		System.out.print(" Declared Fields: ");
+		System.out.println(Arrays.toString(declaredFields));
+		
+		try {
+			Method oneMethod = userClass.getMethod("getLogin");
+			System.out.println("oneMethod: " +oneMethod);
+			
+			
+							
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	
+	}
+}
